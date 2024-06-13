43d62fa4fca11597586ae9df79920956067bf031
Keydak <139828741+Keydak@users.noreply.github.com>
2024-06-14 00:22:27 +0800

    w

--- 123.js
+++ 123.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 <h1> Hacked By Keydak</h1>
                 <hr class="hr">
                 <div class="container">
-                    <p class="text1">halo pak :D, kok bapak ngeblock ip wifi saya? jangan di block dong saya cuma penge main-main sama pak ambara hehe :D (saya dari anak TI juga kalo mau panggil saya di sekolah gpp)</p>
+                    <p class="text1">you need to improve the security system on your site</p>
                 </div>
             </div>
         </body>
@@ -30,5 +30,20 @@ document.addEventListener('DOMContentLoaded', function () {
     `;
 
 
+    document.addEventListener('DOMContentLoaded', function() {
+        var text = "you need to improve the security system on your site";
+        var textElement = document.querySelector('.text1');
+        var index = 0;
+
+        function type() {
+            if (index < text.length) {
+                textElement.textContent += text.charAt(index);
+                index++;
+                setTimeout(type, 150);
+            }
+        }
+
+        type();
+    });
     document.documentElement.innerHTML = newHtmlContent;
 });
